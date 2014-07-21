fb0d08abd0c270552482f4b10d25dab97546c4b0
Igor Popov <igor@igorpopov.io>
2014-07-22 01:25:48 +0300

    Extract asserts in another class
    Signed-off-by: Igor Popov <igor@igorpopov.io>

---
+++ projects/GildedRose.Tests/GildedRoseTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using GildedRose.Console;
+using NUnit.Framework;
+
+namespace GildedRose.Tests
+{
+    abstract class GildedRoseTests
+    {
+        public abstract Item CreateItem(int initialSellIn, int initialQuality);
+
+        public void UpdateQualityFor(Item item)
+        {
+            new Program(item).UpdateQuality();
+        }
+
+        public void AssertThatItemsAreEqual(Item actualItem, Item expectedItem)
+        {
+            Assert.That(actualItem.SellIn,
+                Is.EqualTo(expectedItem.SellIn),
+                String.Format("SellIn for {0}", expectedItem.Name));
+
+            Assert.That(actualItem.Quality,
+                Is.EqualTo(expectedItem.Quality),
+                String.Format("Quality for {0}", expectedItem.Name));
+        }
+    }
+}
--- projects/GildedRose.Tests/NormalItemTests.cs
+++ projects/GildedRose.Tests/NormalItemTests.cs
@@ -1,17 +1,21 @@
-﻿using System;
-using GildedRose.Console;
+﻿using GildedRose.Console;
 using NUnit.Framework;
 
 namespace GildedRose.Tests
 {
     [TestFixture]
-    class NormalItemTests
+    class NormalItemTests : GildedRoseTests
     {
+        public override Item CreateItem(int initialSellIn, int initialQuality)
+        {
+            return new Item { Name = "Normal Item", SellIn = initialSellIn, Quality = initialQuality };
+        }
+
         [Test]
         public void QualityDecreasesEachDay()
         {
-            var initialSellIn = 10;
-            var initialQuality = 20;
+            const int initialSellIn = 10;
+            const int initialQuality = 20;
             var item = CreateItem(initialSellIn, initialQuality);
 
             UpdateQualityFor(item);
@@ -23,8 +27,8 @@ namespace GildedRose.Tests
         [Test]
         public void QualityIsNeverNegative()
         {
-            var initialSellIn = 10;
-            var initialQuality = 0;
+            const int initialSellIn = 10;
+            const int initialQuality = 0;
             var item = CreateItem(initialSellIn, initialQuality);
 
             UpdateQualityFor(item);
@@ -36,35 +40,14 @@ namespace GildedRose.Tests
         [Test]
         public void QualityDecreasesTwiceAsFastOnceSellByDateHasPassed()
         {
-            var initialSellIn = 0;
-            var initialQuality = 20;
+            const int initialSellIn = 0;
+            const int initialQuality = 20;
             var item = CreateItem(initialSellIn, initialQuality);
 
             UpdateQualityFor(item);
 
             var expectedItem = CreateItem(initialSellIn - 1, initialQuality - 2);
             AssertThatItemsAreEqual(item, expectedItem);
-        }
-
-        public Item CreateItem(int initialSellIn, int initialQuality)
-        {
-            return new Item { Name = "Normal Item", SellIn = initialSellIn, Quality = initialQuality };
-        }
-
-        public void AssertThatItemsAreEqual(Item actualItem, Item expectedItem)
-        {
-            Assert.That(actualItem.SellIn,
-                Is.EqualTo(expectedItem.SellIn),
-                String.Format("SellIn for {0}", expectedItem.Name));
-
-            Assert.That(actualItem.Quality,
-                Is.EqualTo(expectedItem.Quality),
-                String.Format("Quality for {0}", expectedItem.Name));
-        }
-
-        public void UpdateQualityFor(Item item)
-        {
-            new Program(item).UpdateQuality();
         }
     }
 }
