a59e78ecdcac756fd9a316ad2400ea970cb7a304
Igor Popov <igor@igorpopov.io>
2014-07-22 00:19:14 +0300

    Setup solution
    Signed-off-by: Igor Popov <igor@igorpopov.io>

---
+++ projects/GildedRose.Tests/GildedRoseTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace GildedRose.Tests
+{
+    [TestFixture]
+    class GildedRoseTests
+    {
+        [Test]
+        public void GildedRoseItems()
+        {
+            Assert.Fail("You must write your own tests!");
+        }
+    }
+}
