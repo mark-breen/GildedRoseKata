56792103e155b7f1801384914647b35603a55c3a
Igor Popov <igor@igorpopov.io>
2014-07-22 01:51:30 +0300

    Add tests for sulfuras
    Signed-off-by: Igor Popov <igor@igorpopov.io>

---
+++ projects/GildedRose.Tests/SulfurasTests.cs
@@ -0,0 +1,27 @@
+﻿using GildedRose.Console;
+using NUnit.Framework;
+
+namespace GildedRose.Tests
+{
+    [TestFixture]
+    class SulfurasTests : GildedRoseTests
+    {
+        public override Item CreateItem(int sellIn, int quality)
+        {
+            return new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = sellIn, Quality = quality };
+        }
+
+        [Test]
+        public void QualityAndSellInAlwaysStayTheSame()
+        {
+            const int initialSellIn = 10;
+            const int initialQuality = 20;
+            var item = CreateItem(initialSellIn, initialQuality);
+
+            UpdateQualityFor(item);
+
+            var expectedItem = CreateItem(initialSellIn, initialQuality);
+            AssertThatItemsAreEqual(item, expectedItem);
+        }
+    }
+}
