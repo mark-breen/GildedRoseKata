23b9cee9d39f8a068fd900d4be393834d1c22202
Igor Popov <igor@igorpopov.io>
2014-07-22 22:17:50 +0300

    Remove duplicate code from BackstagePassesTests
    Signed-off-by: Igor Popov <igor@igorpopov.io>

--- projects/GildedRose.Tests/BackstagePassesTests.cs
+++ projects/GildedRose.Tests/BackstagePassesTests.cs
@@ -4,76 +4,27 @@ using NUnit.Framework;
 namespace GildedRose.Tests
 {
     [TestFixture]
-    class BackstagePassesTests : GildedRoseTests
+    internal class BackstagePassesTests : GildedRoseTests
     {
         public override Item CreateItem(int sellIn, int quality)
         {
             return new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = sellIn, Quality = quality };
         }
 
-        [Test]
-        public void QualityIncreasesNormallyMoreThanTenDaysBeforeConcert()
+        [TestCase(20, 21, 11, 10, "quality increases normally more than 10 days before the concert")]
+        [TestCase(20, 22, 10, 9, "quality increases twice as fast 10 days before the concert")]
+        [TestCase(20, 22, 6, 5, "quality increases twice as fast more than 5 days before the concert")]
+        [TestCase(20, 23, 5, 4, "quality increases three times as fast 5 days before the concert")]
+        [TestCase(20, 0, 0, -1, "quality drops to 0 after the concert")]
+        public void QualityIsUpdated(int initialQuality, int expectedQuality,
+            int initialSellIn, int expectedSellIn, string message)
         {
-            const int initialSellIn = 11;
-            const int initialQuality = 20;
             var item = CreateItem(initialSellIn, initialQuality);
 
             UpdateQualityFor(item);
 
-            var expectedItem = CreateItem(initialSellIn - 1, initialQuality + 1);
-            AssertThatItemsAreEqual(item, expectedItem);
-        }
-
-        [Test]
-        public void QualityIncreasesTwiceAsFastTenDaysBeforeConcert()
-        {
-            const int initialSellIn = 10;
-            const int initialQuality = 20;
-            var item = CreateItem(initialSellIn, initialQuality);
-
-            UpdateQualityFor(item);
-
-            var expectedItem = CreateItem(initialSellIn - 1, initialQuality + 2);
-            AssertThatItemsAreEqual(item, expectedItem);
-        }
-
-        [Test]
-        public void QualityIncreasesTwiceAsFastMoreThanFiveDaysBeforeConcert()
-        {
-            const int initialSellIn = 6;
-            const int initialQuality = 20;
-            var item = CreateItem(initialSellIn, initialQuality);
-
-            UpdateQualityFor(item);
-
-            var expectedItem = CreateItem(initialSellIn - 1, initialQuality + 2);
-            AssertThatItemsAreEqual(item, expectedItem);
-        }
-
-        [Test]
-        public void QualityIncreasesThriceAsFastFiveDaysBeforeConcert()
-        {
-            const int initialSellIn = 5;
-            const int initialQuality = 20;
-            var item = CreateItem(initialSellIn, initialQuality);
-
-            UpdateQualityFor(item);
-
-            var expectedItem = CreateItem(initialSellIn - 1, initialQuality + 3);
-            AssertThatItemsAreEqual(item, expectedItem);
-        }
-
-        [Test]
-        public void QualityDropsToZeroAfterConcert()
-        {
-            const int initialSellIn = 0;
-            const int initialQuality = 20;
-            var item = CreateItem(initialSellIn, initialQuality);
-
-            UpdateQualityFor(item);
-
-            var expectedItem = CreateItem(initialSellIn - 1, 0);
-            AssertThatItemsAreEqual(item, expectedItem);
+            var expectedItem = CreateItem(expectedSellIn, expectedQuality);
+            AssertThatItemsAreEqual(item, expectedItem, message);
         }
     }
 }
