e8478335831a5d824ca55c89b54d4dc2f06d6de9
Igor Popov <igor@igorpopov.io>
2014-07-26 13:09:03 +0300

    Make UpdateQuality and Items static
    Signed-off-by: Igor Popov <igor@igorpopov.io>

--- projects/GildedRose.Console/Program.cs
+++ projects/GildedRose.Console/Program.cs
@@ -5,29 +5,24 @@ namespace GildedRose.Console
 {
     public class Program
     {
-        IList<Item> Items;
+        static IList<Item> Items;
         static void Main(string[] args)
         {
             System.Console.WriteLine("OMGHAI!");
 
-            var app = new Program()
+            Items = new List<Item>
             {
-                Items = new List<Item>
-                                          {
-                                              new NormalItem(name: "+5 Dexterity Vest", sellIn: 10, quality: 20),
-                                              new AgedBrie(sellIn: 2, quality: 0),
-                                              new NormalItem(name: "Elixir of the Mongoose", sellIn: 5, quality: 7),
-                                              new Sulfuras(sellIn: 0, quality: 80),
-                                              new BackstagePasses (sellIn: 15, quality: 20),
-                                              new ImprovedItem {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
-                                          }
-
+                new NormalItem(name: "+5 Dexterity Vest", sellIn: 10, quality: 20),
+                new AgedBrie(sellIn: 2, quality: 0),
+                new NormalItem(name: "Elixir of the Mongoose", sellIn: 5, quality: 7),
+                new Sulfuras(sellIn: 0, quality: 80),
+                new BackstagePasses(sellIn: 15, quality: 20),
+                new ImprovedItem {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
             };
 
-            app.UpdateQuality();
+            UpdateQuality();
 
             System.Console.ReadKey();
-
         }
 
         public Program() { }
@@ -37,7 +32,7 @@ namespace GildedRose.Console
             Items = new List<Item> { item };
         }
 
-        public void UpdateQuality()
+        public static void UpdateQuality()
         {
             foreach (var item in Items)
                 UpdateQualityFor(item);
