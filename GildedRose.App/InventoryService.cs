a40fcad4bb3d9b513e8dc1efa8d83437da218fbb
mark-breen <m.breen@zengenti.com>
2017-11-13 12:52:16 +0000

    Removed legacy code

--- GildedRose.App/InventoryService.cs
+++ GildedRose.App/InventoryService.cs
@@ -5,10 +5,6 @@ namespace GildedRose.App
 {
     public class InventoryService
     {
-        private readonly HashSet<string> _nonRefactoredItems = new HashSet<string>
-        {
-        };
-
         private readonly Dictionary<string, IUpdateStrategy> _nonStandardUpdateStrategies = new Dictionary<string, IUpdateStrategy>
         {
             { "Aged Brie", new QualityLimitedIncreasingQualityStrategy() },
@@ -20,92 +16,12 @@ namespace GildedRose.App
         {
             var inventoryItem = new InventoryItem(name, sellIn, quality);
 
-            if (_nonRefactoredItems.Contains(inventoryItem.Name))
-            {
-                return LegacyUpdateMethod(inventoryItem);
-            }
-
             if (_nonStandardUpdateStrategies.ContainsKey(inventoryItem.Name))
             {
                 return _nonStandardUpdateStrategies[inventoryItem.Name].Update(inventoryItem);
             }
 
             return new StandardItemUpdateStrategy().Update(inventoryItem);
-        }
-
-        private static InventoryItem LegacyUpdateMethod(InventoryItem inventoryItem)
-        {
-            if (inventoryItem.Name != "Aged Brie" && inventoryItem.Name != "Backstage passes to a TAFKAL80ETC concert")
-            {
-                if (inventoryItem.Quality > 0)
-                {
-                    if (inventoryItem.Name != "Sulfuras, Hand of Ragnaros")
-                    {
-                        inventoryItem.Quality = inventoryItem.Quality - 1;
-                    }
-                }
-            }
-            else
-            {
-                if (inventoryItem.Quality < 50)
-                {
-                    inventoryItem.Quality = inventoryItem.Quality + 1;
-
-                    if (inventoryItem.Name == "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (inventoryItem.SellIn < 11)
-                        {
-                            if (inventoryItem.Quality < 50)
-                            {
-                                inventoryItem.Quality = inventoryItem.Quality + 1;
-                            }
-                        }
-
-                        if (inventoryItem.SellIn < 6)
-                        {
-                            if (inventoryItem.Quality < 50)
-                            {
-                                inventoryItem.Quality = inventoryItem.Quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (inventoryItem.Name != "Sulfuras, Hand of Ragnaros")
-            {
-                inventoryItem.SellIn = inventoryItem.SellIn - 1;
-            }
-
-            if (inventoryItem.SellIn < 0)
-            {
-                if (inventoryItem.Name != "Aged Brie")
-                {
-                    if (inventoryItem.Name != "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (inventoryItem.Quality > 0)
-                        {
-                            if (inventoryItem.Name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                inventoryItem.Quality = inventoryItem.Quality - 1;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        inventoryItem.Quality = inventoryItem.Quality - inventoryItem.Quality;
-                    }
-                }
-                else
-                {
-                    if (inventoryItem.Quality < 50)
-                    {
-                        inventoryItem.Quality = inventoryItem.Quality + 1;
-                    }
-                }
-            }
-
-            return inventoryItem;
         }
     }
 
